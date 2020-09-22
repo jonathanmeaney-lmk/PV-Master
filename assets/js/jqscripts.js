0fb5ac29c229f200859edc81c1180649fcf757ab
Jonathan <jonathanmeaney1982@gmail.com>
2020-09-22 14:26:34 +0000

    verbs added

--- assets/js/jqscripts.js
+++ assets/js/jqscripts.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 
-    $( "#verb-one" ).click(function() {
-        $( "#example-one" ).toggle()
+// Toggle to show/hide verb example boxes 
+
+    $("#verb-one").click(function() {
+        $("#example-one").toggle()
+    })
+
+     $("#verb-two").click(function() {
+        $("#example-two").toggle()
+    })
+    
+     $("#verb-three").click(function() {
+        $("#example-three").toggle()
+    })
+
+     $("#verb-four").click(function() {
+        $("#example-four").toggle()
+    })
+
+     $("#verb-five").click(function() {
+        $("#example-five").toggle()
+    })
+    
+     $("#verb-six").click(function() {
+        $("#example-six").toggle()
     })
 
     
