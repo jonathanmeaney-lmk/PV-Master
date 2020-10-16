6719060a154322b4716d9810b5dfe90ed848c15c
Jonathan <jonathanmeaney1982@gmail.com>
2020-10-16 09:54:16 +0000

    quiz placeholder container  added

---
+++ assets/js/overQuiz.js
@@ -0,0 +1,12 @@
+const quizArray = [
+    
+    {
+        quizSentence: "The report has some errors. We need to:",
+        answers: [
+            { text: "look over it", correct: true },
+            { text: "get over it", correct: false },
+            { text: "go into it", correct: false },
+        ],
+    }
+
+]
